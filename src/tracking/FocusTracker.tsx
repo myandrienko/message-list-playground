f4d54fdfa1921653678e6a6341bbcd8956398bbb
Matvei Andrienko <m.y.andrienko@outlook.com>
2025-07-07 23:41:15 +0200

    prevent focus ring from flying between trackers

--- src/tracking/FocusTracker.tsx
+++ src/tracking/FocusTracker.tsx
@@ -1,43 +1,79 @@
 import {
   useState,
+  type ComponentProps,
   type CSSProperties,
+  type ElementType,
   type FocusEvent,
   type PropsWithChildren,
 } from "react";
 import styles from "./FocusTracker.module.css";
 import { nanoid } from "nanoid";
+import clsx from "clsx";
+
+export type FocusTrackerProps<T extends ElementType = "div"> = {
+  as?: T;
+} & ComponentProps<T>;
 
 interface FocusRingAnimationParams {
   key: string;
-  from: DOMRect;
-  to: DOMRect;
+  from: Rect;
+  to: Rect;
 }
 
-export function FocusTracker(props: PropsWithChildren) {
+interface Rect {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+}
+
+export function FocusTracker<T extends ElementType = "div">(
+  props: PropsWithChildren<FocusTrackerProps<T>>
+) {
+  const { as, ...rest } = props;
   const [animationParams, setAnimationParams] =
     useState<FocusRingAnimationParams | null>(null);
 
   const handleFocus = (event: FocusEvent) => {
-    if (event.target && event.relatedTarget) {
-      const rectFrom = event.relatedTarget.getBoundingClientRect();
-      const rectTo = event.target.getBoundingClientRect();
+    const from = event.relatedTarget;
+    const to = event.target;
+
+    if (
+      from?.closest("[data-focus-tracker]") !== event.currentTarget ||
+      to?.closest("[data-focus-tracker]") !== event.currentTarget
+    ) {
+      return;
+    }
+
+    if (
+      event.target instanceof HTMLElement &&
+      event.relatedTarget instanceof HTMLElement
+    ) {
+      const rectFrom = getRect(event.relatedTarget);
+      const rectTo = getRect(event.target);
       setAnimationParams({ key: nanoid(), from: rectFrom, to: rectTo });
     } else {
       setAnimationParams(null);
     }
   };
 
+  const Component = as ?? "div";
+
   return (
-    <div
+    <Component
+      {...rest}
+      className={clsx(styles.root, rest.className)}
       style={
         animationParams
           ? {
+              ...rest.style,
               ...rectToCustomCssProps("focus-ring-from", animationParams.from),
               ...rectToCustomCssProps("focus-ring-to", animationParams.to),
             }
-          : undefined
+          : rest.style
       }
       onFocus={handleFocus}
+      data-focus-tracker=""
     >
       {props.children}
       {animationParams && (
@@ -47,11 +83,20 @@ export function FocusTracker(props: PropsWithChildren) {
           onAnimationEnd={() => setAnimationParams(null)}
         />
       )}
-    </div>
+    </Component>
   );
 }
 
-function rectToCustomCssProps(prefix: string, rect: DOMRect) {
+function getRect(el: HTMLElement) {
+  return {
+    x: el.offsetLeft,
+    y: el.offsetTop,
+    width: el.offsetWidth,
+    height: el.offsetHeight,
+  };
+}
+
+function rectToCustomCssProps(prefix: string, rect: Rect) {
   return {
     [`--${prefix}-x`]: `${rect.x}`,
     [`--${prefix}-y`]: `${rect.y}`,
